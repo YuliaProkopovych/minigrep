86558af1649176cd77a3cef6531bf16fe3965e86
Yulia <schonebud@gmail.com>
2023-04-24 09:04:37 +0300

    Separate file handling logic into run function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 struct Config {
   query: String,
@@ -20,6 +21,14 @@ impl Config {
   }
 }
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let contents = fs::read_to_string(config.file_path)?;
+
+  println!("With text:\n{contents}");
+
+  Ok(())
+}
+
 fn main() {
   let args: Vec<String> = env::args().collect();
 
@@ -31,8 +40,8 @@ fn main() {
   println!("Searching for {}", config.query);
   println!("In file {}", config.file_path);
 
-  let contents = fs::read_to_string(config.file_path)
-  .expect("Should have been able to read the file");
-
-  println!("With text:\n{contents}");
+  if let Err(e) = run(config) {
+    println!("Application error: {e}");
+    process::exit(1);
+  }
 }
